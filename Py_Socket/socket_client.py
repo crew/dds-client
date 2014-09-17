f107067fb90198bb7999b04936654c9c5bba75d8
Alex Grundwerg <agrundw@gmail.com>
2014-09-16 23:10:13 -0400

    updateing to work with varible JSON stuffs

--- Py_Socket/socket_client.py
+++ Py_Socket/socket_client.py
@@ -5,9 +5,50 @@ import socket, select, string, sys, time, json
 
 from message import Message
 
-def prompt():
-    sys.stdout.write('<You> ')
-    sys.stdout.flush()
+def Input():
+    while True:
+        print("""
+        1. getSlides()
+        2. heartbeat()
+        3. getHardware()
+        4. List Groups that a User is in
+        5. List Users that are in 12 or more groups
+        6. Suggest a cleanup
+        7. Quit
+        """)
+        selection = raw_input("Select an option [1-7] \n")
+        if selection == "1":
+            jsonRequest = getSlides()
+            break
+        elif selection == "2":
+            jsonRequest = heartbeat()
+            break
+        elif selection == "3":
+            jsonRequest = getHardware()
+            break
+        elif selection == "4":
+            userInGroup(Users)
+        elif selection == "5":
+            moreThan12(Users)
+        elif selection == "6":
+            suggest(Groups, Users)
+        elif selection == "7":
+            loop = False
+        else:
+            print "nope"
+    return jsonRequest
+
+def getSlides():
+    jsonRequest = {}
+    return jsonRequest
+
+def heartbeat():
+    jsonaAnswer = {}
+    return jsonRequest
+
+def getHardware():
+    jsonRequest = {}
+    return jsonRequest
 
 def connect(host, port): 
     while True:
@@ -39,8 +80,7 @@ def main():
     
     while run:
         print 'Connected to remote host. Start sending messages'
-        prompt()
-
+	#Input()
         socket_list = [sys.stdin, s]
         # Get the list sockets which are readable
         read_sockets, write_sockets, error_sockets = select.select(socket_list , [], [])
@@ -61,9 +101,8 @@ def main():
              
             #user entered a message
             else :
-                msg = sys.stdin.readline()
-                s.send(msg)
-                prompt()
+                msg = Input()
+                s.send(msg.toJSON())
 
 if __name__ == "__main__":
     main()
