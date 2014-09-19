ab143e1c6d18b17f7bfd90ce6ca127ba615f93e2
dalelyunas <dvad123@gmail.com>
2014-09-18 22:03:41 -0400

    Added the ability to input and send messages from the server to the client, which then displays it as a label

--- Client/pythonsocket.py
+++ Client/pythonsocket.py
@@ -14,22 +14,29 @@ class WSHandler(tornado.websocket.WebSocketHandler):
     def check_origin(self, origin):
         return True
     
+    #opens a loop that waits for raw_input and sends it
     def open(self):
         print 'new connection'
         self.write_message("Hello World")
+        while True:
+            self.loop()
         
     def on_message(self, message):
         print 'message received %s' % message
  
     def on_close(self):
         print 'connection closed'
+        
+    def loop(self):
+        message = raw_input('Enter message: ')
+        self.write_message(message)
  
- 
+#creates tornado server
 application = tornado.web.Application([
     (r'/ws', WSHandler),
 ])
  
- 
+#starts tornado server
 http_server = tornado.httpserver.HTTPServer(application)
 http_server.listen(8888)
 tornado.ioloop.IOLoop.instance().start()
