bc01d2e45a9ce0be03e07eb611f5b2b369436d40
belph <peblairman@gmail.com>
2014-09-27 00:16:03 -0400

    Added GTK display file and loading screen html.

---
+++ Client/gtkDisplay.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+# GTK-Based Fullscreen Web Display Client
+#    Description: Client accepting pushes
+#    from server to load webpages. Intended
+#    for use in DDS System
+
+# Extension of python-gtk-webkit presentation program
+#                   Copyright (C) 2009 by Akkana Peck.
+# (Out of compliance with Peck's copyright, the following
+#       is released under GPL v2)
+
+# Copyright (C) 2014 Northeastern University CCIS Crew
+
+
+import sys
+import gobject
+import gtk
+import webkit
+import threading
+from slide import Slide
+import Queue
+
+#for testing
+import time
+#sys.settrace
+
+# Represents fullscreen gtk window displaying
+#   the URL provided to it
+# Note: URL must begin with prefix (e.g. "http://")
+class WebBrowser(gtk.Window):
+    def __init__(self, url):
+        gtk.Window.__init__(self)
+        self.fullscreen()
+
+        self._browser= webkit.WebView()
+        self.add(self._browser)
+        self.connect('destroy', gtk.main_quit)
+
+        self._browser.open(url)
+        self.show_all()
+
+        gobject.threads_init()
+
+    def updatePage(self,url):
+        self.connect("destroy", gtk.main_quit)
+        self._browser.open(url)
+        self.show_all()
+
+# Class Definition of Main Program Thread
+#   (responsible for running gtk.main())
+# Arguments:
+#       url=(String) Initial URL to display
+class MainThread (threading.Thread):
+    def __init__(self, url):
+        #global webbrowser
+        threading.Thread.__init__(self)
+        self.threadID = 1
+        self.name = "Main GTK"
+        self.webBrowser = WebBrowser(url)
+
+    def updatePage(self,url):
+        self.webBrowser = WebBrowser(url)
+
+
+# Called by page-updating thread to change
+#   the page displayed by the WebBrowser
+#   object.
+# Arguments:
+#       url=(String) The new page to display
+def openPage(url):
+    global mainThread
+    mainThread.webBrowser.updatePage(url)
+
+#test
+def openPageTest():
+    time.sleep(5)
+    openPage("http://dds-wp.ccs.neu.edu/?slide=test-ccis-tutoring&pie_name=chocolate")
+
+# Class Definition of Page-Updating Thread
+
+# TODO: Integrate with existing queues
+class PageUpdateThread (threading.Thread):
+    def __init__(self, queue=None):
+        threading.Thread.__init__(self)
+        self.threadID = 1
+        self.name = "Page Update"
+        self.queue = queue
+    def run(self):
+        while 1:
+            currentSlide = self.queue.get()
+            openPage(currentSlide.url)
+            time.sleep(currentSlide.duration)
+            self.queue.put(currentSlide)
+
+# Allows initial bash invocation to load
+#      webpage passed as argument
+#if __name__ == "__main__":
+    #if len(sys.argv) <= 1 :
+    #    print("Usage:", sys.argv[0], "url")
+    #    sys.exit(0)
+
+testQueue = Queue.Queue(100)
+testQueue.put(Slide("http://facebook.com", 5))
+testQueue.put(Slide("http://dds-wp.ccs.neu.edu/?slide=test-ccis-tutoring&pie_name=chocolate", 10))
+
+
+mainThread = MainThread("/loading_screen/index.html")
+pageUpdateThread = PageUpdateThread(testQueue)
+
+pageUpdateThread.start()
+mainThread.start()
+
+gtk.main()    
+
+# Called on program start and contains
+#   gtk.main() (Runs until gtk.main_quit()...
+#   i.e. for the program's duration)
+#def mainThread(url):
+#    gobject.threads_init()
+#    webbrowser = WebBrowser(sys.argv[1])
+#   gtk.main()
+
+
+
+# Refreshing:
+# while True:
+#   time.sleep(10)
---
+++ Client/slide.py
@@ -0,0 +1,18 @@
+# Slide Class Definition
+# PARAMETERS
+# 	| <String> url: The url to load when displaying the slide
+# 	| <Integer> duration: The length of time to display the slide, in seconds
+# 	| <Function> action: (optional) Action associated with the slide (NFC? :o)
+
+# (c) Northeastern University Crew 2014
+class Slide():
+	def __init__(self, url, duration, action=None):
+		"""
+
+		:param url: String
+		:param duration: Integer
+		:param action: Function
+		"""
+		self.url = url
+		self.duration = duration
+		self.action = action
