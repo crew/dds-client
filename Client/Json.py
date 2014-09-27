180773f8fc7bd5c91abf4606eb9c45961a299df3
Jonah-Min <jonahm2472@gmail.com>
2014-09-27 18:09:37 -0400

    Added Key Presses
    Now adds what's in the queue when the "a" key is pressed. If the "r" key
    is pressed, puts the last slide into the queue, then removes it from the
    Json. Ready to be implemented and tweaked however needed.

--- Client/Json.py
+++ Client/Json.py
@@ -47,17 +47,32 @@ obj = {
         }
     ]
 }
+
 #Loops through the Json and outputs the data
 #Once it reaches the end of the data it resets
+#If "a" key is pressed, then it adds the next slide in the queue to the JSON
+#if "r" key is pressed, puts fram back into queue, then it removes the last slide in the JSON
+#prints "at last frame!" if you press r to the last frame
 x = 0
 while x < len(obj["actions"]):
         print obj["actions"][x]["type"] + ": " + obj["actions"][x]["location"]
         target_time = datetime.datetime.now() + datetime.timedelta(seconds = obj["actions"][x]["duration"])
         while(datetime.datetime.now() < target_time):
-            if not q.empty():
-                currentMessage = q.get()
-                obj["actions"].append(currentMessage)
             pass
+            if msvcrt.kbhit():
+                key = msvcrt.getch()
+                if key == 'a':
+                    if not q.empty():
+                        currentMessage = q.get()
+                        obj["actions"].append(currentMessage)
+                elif key == 'r':
+                    if len(obj["actions"]) > 1:
+                            q.put(obj["actions"][(len(obj["actions"]) - 1)])
+                            del obj["actions"][(len(obj["actions"])-1)]
+                    else:
+                            print "at last frame!"
+                else:
+                    pass
         if x < (len(obj["actions"]) - 1):
             x+=1
         else:            
