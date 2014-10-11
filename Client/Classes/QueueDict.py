716ad54126ea01cc55ce28f4dbdcfa4ef815f5fa
aelci <elci.a@husky.neu.edu>
2014-10-10 20:49:17 -0400

    Changed queues package and class to QueueDict package and class. Changed relevant occurrences in Classes/threads.py and main.py

---
+++ Client/Classes/QueueDict.py
@@ -0,0 +1,28 @@
+import Queue
+from message import Message
+
+class QueueDict:
+    """
+    A String to Queue dictionary
+    
+    :var _queues : Dictionary. Dictionary of queues
+    :var _queueSize : Integer. Queue size in _queues
+
+    """
+
+    def __init__(self, queueSize = 100):
+		self._queues = {}
+        self._queueSize = queueSize
+    
+
+    """
+    :param <name> : String. Markup name of a queue.
+    """
+    def getQueue(self, name):
+        return self._queues[name]
+
+    def __getitem__(self, name):
+        return self.getQueue(name)
+
+    def addQueue(self, name):
+		self._queues[name] = Queue.Queue(queueSize)
