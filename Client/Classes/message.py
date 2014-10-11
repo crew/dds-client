eb370f1b222261234d567a7ae1af7f0dc850a5e0
c0d3d <nlocketz@gmail.com>
2014-10-10 22:38:30 -0400

    Moved files due to gross setup

--- Client/Classes/message.py
+++ Client/Classes/message.py
@@ -2,10 +2,16 @@ import json
 
 class Message:
     """"A message object to be passed between threads via Queue"""
-    def __init__(self, src, dest, action, content):
+#src is the name of the src for this message
+#dest is the name of the pie || server
+#pluginDest is the name of the plugin that this message should be distributed to
+#action is the function that the destPlugin should run
+#content is the payload of this message, the plugin should know how to interpret the content of this message
+    def __init__(self, src, dest, pluginDest, action, content):
         """src is the name of the thread, dest is the goal thread, action is a string that can run a function, content is a dictionary"""
         self.src = src
         self.dest = dest
+	self.pluginDest = pluginDest
         self.action = action
         self.content = content
 
